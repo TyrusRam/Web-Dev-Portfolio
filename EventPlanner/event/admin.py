b181ec371233618a36be29c08e354dc2e9df4617
TyrusRam <tyrusramirez@gmail.com>
2025-01-10 04:54:41 -0800

    Delete EventPlanner directory

--- EventPlanner/event/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User, Events, RSVP
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Events)
-admin.site.register(RSVP)
